cf21850b43b2ad06337b9144d03c9d033e9ebf3a
khr898 <kaveen.himash@outlook.com>
2026-08-05 10:42:16 +0530

    Update ntfsmac formula to v2.0.050826
    Replaces bogus v1.1.020826 tag (no release was cut Aug 2). sha256 stays
    placeholder until the v2.0.050826 release tarball is uploaded.

--- Formula/ntfsmac.rb
+++ Formula/ntfsmac.rb
@@ -3,7 +3,7 @@ class Ntfsmac < Formula
   homepage "https://github.com/khr898/ntfsmac"
   # By default, download the prebuilt CLI binaries and scripts packaged in the GitHub release
   # to make installation extremely fast and robust (no local compiler toolchains required).
-  url "https://github.com/khr898/ntfsmac/releases/download/v1.1.020826/ntfsmac-cli.tar.gz"
+  url "https://github.com/khr898/ntfsmac/releases/download/v2.0.050826/ntfsmac-cli.tar.gz"
   sha256 "<FILL_SHA256>"
   license "MIT"
 
